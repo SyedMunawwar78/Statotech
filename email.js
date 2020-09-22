c3d3bdf55d270d39f4baa831ae84f4636e3305b3
syeda ayeen uz zehra <say_era@syedas-MacBook.local>
2020-09-22 15:27:36 -0500

    validations using juery

--- email.js
+++ email.js
@@ -1,24 +1,39 @@
 function sendEmail() {
    
     var name=$('#name').val()
-    /*if(name==''){
-        alert('Please enter your name')
+    
+    if(name==''){
+        $('.name-message').removeClass('alert-display')
         return
-    }*/
+        
+    }else{
+        $('.name-message').addClass('alert-display')
+    }
     name="Enquiry from "+name;
     var email=$('#email').val()
-    /*if(email==''){
-        alert('Please enter your email');
+    var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
+    if(email==''){
+        $('.email-message').removeClass('alert-display')
         return
-    }*/
+    }else{
+        $('.email-message').addClass('alert-display')
+
+    }
+    if(!pattern.test(email)){
+        $('.email-perfect').removeClass('alert-display')
+        return
+    }else{
+        $('.email-perfect').addClass('alert-display')
+    }
     name="Enquiry from "+name+"-"+email;
     var message=$('#message').val()
-    /*if(message===''){
-        alert('Please enter a message');
+    if(message===''){
+        $('.area-message').removeClass('alert-display')
         return
-    }*/
+    }else{
+        $('.area-message').addClass('alert-display')
+    }
 
-alert(email)
     Email.send({
     Host: "smtpout.secureserver.net",
     Username : "info@statotechsolutions.com.au",
@@ -28,6 +43,6 @@ alert(email)
     Subject :name,
     Body : message,
     }).then(
-          message => console.log("Email Sent Successfully:"+message)
+          message => $('.success-message').removeClass('alert-display')
     );
 }
